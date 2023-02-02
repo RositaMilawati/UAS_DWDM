72121c27deafc9a388ef646ba52c9f05b125f40a
Rosita Milawati <59589009+RositaMilawati@users.noreply.github.com>
2023-02-02 09:49:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if app_mode=='Home':
 elif app_mode == 'Prediction':
     st.image('prediction.png')
     st.write('\n')
-    st.title('Masukkan informasi anda dibawah ini :')
+    st.title('Input information about you :')
     
     st.write('\n')
     age = st.number_input("Age", 0)
