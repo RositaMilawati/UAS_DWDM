2b040331870519f0f1544948828eea7bfc213295
Rosita Milawati <59589009+RositaMilawati@users.noreply.github.com>
2023-02-02 09:32:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,17 @@ def prediction(age, sex, bmi, children, smoker):
 app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
 
 if app_mode=='Home':
-    st.title('Prediksi Pembayaran Premi Asuransi :') 
-    st.title("Aplikasi Prediksi Pembayaran Premi Asuransis Dengan Algoritma Regresi Linier")
+    st.title('Prediksi Biaya Asuransi Kesehatan :')
+    st.image('prediction.png')
+    st.title("Aplikasi Prediksi Pembayaran Asuransi Kesehatan Dengan Algoritma Regresi Linier")
     st.markdown('Dataset :')
     data=pd.read_csv('insurance1.csv')
     st.write(data.head())
 
 elif app_mode == 'Prediction':
+    st.image('prediction.png')
     st.write('\n')
-    st.markdown('Silakan, isi form berikut ini :')
+    st.markdown('Masukkan informasi anda dibawah ini :')
     
     st.write('\n')
     age = st.number_input("Age", 0)
@@ -33,6 +35,6 @@ elif app_mode == 'Prediction':
     smoker = st.number_input("Smoker", 0)
     result =""
     
-    if st.button("KLIK DI SINI UNTUK PREDIKSI"):
+    if st.button("KLIK UNTUK PREDIKSI"):
         result = prediction(age, sex, bmi, children, smoker)
     st.success('Hasil Prediksi = {}'.format(result))
